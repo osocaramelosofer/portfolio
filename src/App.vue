196a15504865ac3b2b74bae30a38042fa3461530
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-04-09 20:35:26 -0600

    feat: searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
+<script setup lang="ts">
+import timezones from 'timezones.json'
+import Fuse from 'fuse.js'
+import type { Timezone } from './types'
+
+const zones = ref<Timezone[]>([])
+const input = ref('')
+const fuse = new Fuse(timezones, {
+  keys: ['utc', 'value', 'text', 'abbr'],
+})
+
+const searchResult = computed(() => {
+  // return timezones.filter(i => i.utc.find(u => u.includes(input.value)))
+  return fuse.search(input.value)
+})
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <RouterView />
-    <TheFooter />
+    <input v-model="input" type="text" text-black>
+    <div>
+      <div v-for="i of searchResult" :key="i.refIndex">
+        {{ i.item.text }}
+      </div>
+    </div>
+
+    <!-- <RouterView />
+    <TheFooter /> -->
   </main>
 </template>
