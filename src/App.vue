cc732c1697d9be5f725a4c2d5f041a7fbaee78fb
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-04-09 21:16:13 -0600

    rework: searchbar + fuse

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
-import timezones from 'timezones.json'
+import timezoneRaw from 'timezones.json'
 import Fuse from 'fuse.js'
 import type { Timezone } from './types'
 
+const timezone = timezoneRaw.flatMap((i) => {
+  return i.utc.map((u) => {
+    return {
+      name: u,
+      offset: i.offset,
+    }
+  })
+})
+
 const zones = ref<Timezone[]>([])
 const input = ref('')
-const fuse = new Fuse(timezones, {
-  keys: ['utc', 'value', 'text', 'abbr'],
+const fuse = new Fuse(timezone, {
+  keys: ['name'],
 })
 
 const searchResult = computed(() => {
-  // return timezones.filter(i => i.utc.find(u => u.includes(input.value)))
   return fuse.search(input.value)
 })
 </script>
@@ -20,7 +28,7 @@ const searchResult = computed(() => {
     <input v-model="input" type="text" text-black>
     <div>
       <div v-for="i of searchResult" :key="i.refIndex">
-        {{ i.item.text }}
+        {{ i.item.name }} - {{ i.item.offset }}
       </div>
     </div>
 
